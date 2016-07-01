8f44d158f29433b74e26db73497aaade6db6eaa5
maftalion <aftalion.matt@gmail.com>
2016-07-01 16:18:21 -0700

    Set up Schema and Database

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT,
+  message varchar(255) NOT NULL,
+  user_id int NOT NULL,
+  room_id int NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
