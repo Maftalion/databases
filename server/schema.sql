7d2f2b1cd082b4350721af740b2062dc6a035695
maftalion <aftalion.matt@gmail.com>
2016-07-02 15:39:39 -0700

    Update schema file

--- server/schema.sql
+++ server/schema.sql
@@ -5,19 +5,21 @@ USE chat;
 
  -- TODO: make things work with a UNIQUE constraint here
 CREATE TABLE users (
-  id varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+  id varchar(255) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE messages (
   /* Describe your table here.*/
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   message varchar(255) NOT NULL,
   username varchar(255) NOT NULL,
-  roomname varchar(255) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (username) REFERENCES users(id)
+  roomname varchar(255) NOT NULL
 );
+
+alter table messages add FOREIGN KEY (username) REFERENCES users(id);
+
+INSERT INTO users (id) VALUES ('anon');
+INSERT INTO messages (message, username, roomname) VALUES('Hello', 'anon', 'lobby');
 
 /* Create other tables and define schemas for them here! */
 
