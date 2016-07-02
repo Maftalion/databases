126b13a2e952ec3116eb064356bde912b607ed44
maftalion <aftalion.matt@gmail.com>
2016-07-02 14:41:52 -0700

    Add client files

---
+++ client/client.js
@@ -0,0 +1,79 @@
+// https://github.com/hackreactor/2016-06-chatterbox-client/blob/solution/client/scripts/refactor.js
+
+app = {
+
+  server: 'https://api.parse.com/1/classes/messages/',
+
+  init: function() {
+    // Get username
+    app.username = window.location.search.substr(10);
+
+    app.onscreenMessages = {};
+
+    // cache some dom references
+    app.$text = $('#message');
+
+    app.loadMsgs();
+    setInterval (app.loadMsgs.bind(app), 1000);
+
+    $('#send').on('submit', app.handleSubmit);
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var message = {
+      username: app.username,
+      text: app.$text.val()
+    };
+
+    app.$text.val('');
+
+    app.sendMsg(message);
+  },
+
+  renderMessage: function(message) {
+    var $user = $('<div>', {class: 'user'}).text(message.username);
+    var $text = $('<div>', {class: 'text'}).text(message.text);
+    var $message = $('<div>', {class: 'chat', 'data-id': message.objectId }).append($user, $text);
+    return $message;
+  },
+
+  displayMessage: function(message) {
+    if (!app.onscreenMessages[message.objectId]) {
+      var $html = app.renderMessage(message);
+      $('#chats').prepend($html);
+      app.onscreenMessages[message.objectId] = true;
+    }
+  },
+
+  displayMessages: function(messages) {
+    for (var i = messages.length; i > 0; i--) {
+      app.displayMessage(messages[i - 1]);
+    }
+  },
+
+  loadMsgs: function() {
+    $.ajax({
+      url: app.server,
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: function(json) {
+        app.displayMessages(json.results);
+      }
+    });
+  },
+
+  sendMsg: function(message) {
+    $.ajax({
+      type: 'POST',
+      url: app.server,
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(json) {
+        message.objectId = json.objectId;
+        app.displayMessage(message);
+      }
+    });
+  },
+};
